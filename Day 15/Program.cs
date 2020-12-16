2d5e603b7a2db79f3239d56e468a229dea2e624d
S Maher <Mahers@hattonacademy.org.uk>
2020-12-16 15:50:20 +0000

    Day 15 part 1

---
+++ Day 15/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] StartingNumbers = File.ReadAllLines("Numbers.txt");
+            Dictionary<string, int> game = new Dictionary<string, int>();
+
+            string[] Numbers = StartingNumbers[0].Split(',');
+            int turn = 1;
+            string lastNumber = "";
+            bool isNew = false;
+            int lastGap = 0;
+            foreach (string number in Numbers)
+            {
+                if(!game.ContainsKey(number))
+                {
+                    isNew = true;
+                }
+                else
+                {
+                    lastGap = turn - game[number];
+                    isNew = false;
+                }
+                game.Add(number, turn++);
+                lastNumber = number;
+            }
+
+            for (int gameCount = game.Count + 1; gameCount <= 2020; gameCount++)
+            {
+                string NewNum = "0";
+                if(!isNew)
+                {
+                    NewNum = Convert.ToString(lastGap);
+                }
+
+                if (!game.ContainsKey(NewNum))
+                {
+                    isNew = true;
+                }
+                else
+                {
+                    lastGap = turn - game[NewNum];
+                }
+                if (game.ContainsKey(NewNum))
+                {
+                    game[NewNum] =  turn++;
+                    isNew = false;
+                }
+                else
+                {
+                    game.Add(NewNum, turn++);
+                    isNew = true;
+                }
+                lastNumber = NewNum;
+            }
+
+            Console.WriteLine(lastNumber);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 16/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] TicketLines = File.ReadAllLines("Tickets.txt");
+            int ticketLine = 0;
+
+            Dictionary<string, string> TicketsCategories = new Dictionary<string, string>();
+
+            while (TicketLines[ticketLine] != "your ticket:")
+            {
+                if (TicketLines[ticketLine] != "")
+                {
+                    string cat = TicketLines[ticketLine].Split(':')[0];
+                    string restrictions = TicketLines[ticketLine].Split(':')[1].Substring(1);
+                    TicketsCategories.Add(cat, restrictions);
+                    
+                }
+                ticketLine++;
+            }
+            while (TicketLines[ticketLine] != "nearby tickets:")
+            {
+                ticketLine++;
+            }
+            ticketLine++;
+            int errorrate = 0;
+            while(ticketLine < TicketLines.Length)
+            {
+                string[] ticketNumbers = TicketLines[ticketLine].Split(',');
+                for (int ticketItem = 0; ticketItem < ticketNumbers.Length; ticketItem++)
+                {
+                    if(!CheckTicketItem(TicketsCategories, ticketNumbers[ticketItem]))
+                    {
+                        errorrate += Convert.ToInt32(ticketNumbers[ticketItem]);
+                    }
+                }
+                ticketLine++;
+            }
+            Console.WriteLine(errorrate);
+            Console.ReadLine();
+        }
+
+        private static bool CheckTicketItem(Dictionary<string, string> ticketsCategories, string v)
+        {
+            int value = Convert.ToInt32(v);
+            foreach (string key in ticketsCategories.Keys)
+            {
+                string[] requirements = ticketsCategories[key].Split(' ');
+                int lowerBound = Convert.ToInt32(requirements[0].Split('-')[0]);
+                int upperBound = Convert.ToInt32(requirements[0].Split('-')[1]);
+                if(value>= lowerBound && value <= upperBound)
+                {
+                    return true;
+                }
+                lowerBound = Convert.ToInt32(requirements[2].Split('-')[0]);
+                upperBound = Convert.ToInt32(requirements[2].Split('-')[1]);
+                if (value >= lowerBound && value <= upperBound)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
